899aea9693325d0b26dcfa5be6f731b63d772e25
azharhanif <83354175+azharhanif@users.noreply.github.com>
2025-06-16 12:45:33 -0400

    Add files via upload

---
+++ starter-game-prog-1-lab-8/Simulation_Part1/CannonBall.java
@@ -0,0 +1,22 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+/**
+ * Write a description of class CannonBall here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class CannonBall extends Actor
+{
+    private Vector2D velocity;
+    
+    public CannonBall()
+    {
+        
+    }
+    
+    public void act() 
+    {
+        // Add your action code here.
+    }    
+}
